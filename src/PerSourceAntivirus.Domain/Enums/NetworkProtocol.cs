c0aba024638e92195e2e5088a1e0fdd2a2ef281a
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-06-16 21:17:14 -0300

    [feat] Creation, implementation and update files in base from Domain\Enums

---
+++ src/PerSourceAntivirus.Domain/Enums/NetworkProtocol.cs
@@ -0,0 +1,9 @@
+namespace PerSourceAntivirus.Domain.Enums;
+
+public enum NetworkProtocol
+{
+    Other = 0,
+    Tcp = 1,
+    Udp = 2,
+    Icmp = 3
+}
---
+++ src/PerSourceAntivirus.Domain/Enums/ScriptType.cs
@@ -0,0 +1,10 @@
+namespace PerSourceAntivirus.Domain.Enums;
+
+public enum ScriptType
+{
+    Unknown = 0,
+    PowerShell = 1,
+    VBScript = 2,
+    Batch = 3,
+    JavaScript = 4
+}
