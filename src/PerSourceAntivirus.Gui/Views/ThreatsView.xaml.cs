f8ad45cc0ca00d7ebd3a190b8a5115b109887543
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-06-21 10:11:24 -0300

    [feat] Creation, implementation and update files in base from .Gui\Views

---
+++ src/PerSourceAntivirus.Gui/Views/ThreatsView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace PerSourceAntivirus.Gui.Views;
+
+public partial class ThreatsView
+{
+    public ThreatsView() => InitializeComponent();
+}
---
+++ src/PerSourceAntivirus.Gui/Views/TimelineView.xaml.cs
@@ -0,0 +1,6 @@
+namespace PerSourceAntivirus.Gui.Views;
+
+public partial class TimelineView
+{
+    public TimelineView() => InitializeComponent();
+}
