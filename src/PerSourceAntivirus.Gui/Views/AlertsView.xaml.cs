7ae812439eba4c03d7eef6c75fabf9b21c932f40
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-06-21 10:08:49 -0300

    [feat] Creation and update files in .Gui\Views with AlertsView.xaml, DashboardView.xaml, ExclusionsView.xaml

---
+++ src/PerSourceAntivirus.Gui/Views/AlertsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PerSourceAntivirus.Gui.Views;
+
+public partial class AlertsView
+{
+    public AlertsView()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ src/PerSourceAntivirus.Gui/Views/DashboardView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace PerSourceAntivirus.Gui.Views;
+
+public partial class DashboardView
+{
+    public DashboardView() => InitializeComponent();
+}
